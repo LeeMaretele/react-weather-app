e4744c2fb73130d15925971b07a95d26189def77
LeeMaretele <leratomaretele81@gmail.com>
2024-07-26 15:01:47 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
+import React from "react";
+import Weather from "./Weather";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Lerato Maretele and is{" "}
-        <a
-          href="https://github.com/LeeMaretele/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Lerato Maretele and is{" "}
+          <a
+            href="https://github.com/LeeMaretele/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
