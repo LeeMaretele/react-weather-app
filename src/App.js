5ca17550ce35d9f3f028d351f432b3f3db80b835
LeeMaretele <leratomaretele81@gmail.com>
2024-07-26 12:48:24 +0200

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Lerato Maretele and is{" "}
+        <a
+          href="https://github.com/LeeMaretele/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
